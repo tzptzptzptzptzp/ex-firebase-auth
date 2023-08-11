c95c97287273a8f852c681393f3bf227fa32dfec
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-11 20:42:09 +0900

    ✨  Googleアカウントで認証する機能を実装

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,6 +1,12 @@
+import { signInWithPopup } from "firebase/auth"
+import { auth, provider } from "../firebase"
+
 export function SignInButton() {
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+  }
   return (
-    <button>
+    <button onClick={signInWithGoogle}>
       <p>login with google</p>
     </button>
   )
